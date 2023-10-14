b947a96f371290e6b431da3f0b83cb4b702d0ca6
Mayendra <mayendraendra@gmail.com>
2023-10-14 18:35:12 +0700

    tugas 3

---
+++ src/components/data/dummydata.js
@@ -0,0 +1,236 @@
+import { AddLocationAltOutlined, BarChart, CloudOutlined, Code, CropRotate, EmailOutlined, Facebook, FavoriteBorder, Instagram, PersonOutlined, PhoneIphone, PieChart, Public, Settings, Twitter, ViewInAr, YouTube } from "@mui/icons-material"
+
+export const navlink = [
+  {
+    url: "/",
+    text: "Home",
+  },
+  {
+    url: "/about",
+    text: "About",
+  },
+  {
+    url: "/experience",
+    text: "Experience",
+  },
+  {
+    url: "/skills",
+    text: "Skills",
+  },
+  {
+    url: "/interest",
+    text: "Interest",
+  },
+  {
+    url: "/awards",
+    text: "Awards",
+  },
+  {
+    url: "/contact",
+    text: "Contact",
+  },
+]
+export const home = [
+  {
+    text: "HALLO SOBAT",
+    name: "MAYENDRA",
+    post: "FRONT END DEVELOPER",
+    design: "TUGAS PORTFOLIO",
+    desc: "Portofolio ini saya buat untuk melengkapi assigments 3",
+  },
+]
+export const about = [
+  {
+    desc: "Nama lengkap saya Mayendra nama lapangan saya lukeng, Sekarang saya berumur 20 tahun, Saya lahir dari orang tua Yuli Epri dan Mida Yanti pada 09 mei 2003 (Sumatra Barat, Indonesia). Saya bisa menguasai bahasa Minang, Mandailing , Aceh , Tapanuli.Pada saat ini saya menempuh Pendidikan di Universitas Malikussaleh di Aceh dengan jurusan Sistem Informasi.",
+    desc1: "Pada Maret 2021, lulus dari bangku SMA dan saya memilih untuk kuliah di Aceh yaitu di Universitas Malikussaleh dengan jurusan sistem informasi walaupun belum ada bahan dan bekal dibidang dunia komputer namun saya memberanikan diri dengan nekat mengambil jurusan ini.",
+    cover: "./images/udak.png",
+  },
+]
+export const experience = [
+  {
+    id: 1,
+    icon: <Settings />,
+    title: "IMAPAS",
+    desc: "Saya pernah Menjabat Sekretaris Umum di Paguyuban IMAPAS",
+  },
+  {
+    id: 2,
+    icon: <CropRotate />,
+    title: "HIMASI",
+    desc: "Saya pernah menjabat Ketua Bidang Perlengkapan pada acara Musyawarah Nasional",
+  },
+  {
+    id: 3,
+    icon: <ViewInAr />,
+    title: "SAR",
+    desc: "Saya pernah menjabat Ketua devisi HUMAS & INFOKOM di UKM SAR UNIMAL",
+  },
+  {
+    id: 4,
+    icon: <PieChart />,
+    title: "IMAPAS",
+    desc: "Saya pernah menjabat Ketua Devisi HUMAS di Paguyuban IMAPAS",
+  },
+  {
+    id: 5,
+    icon: <Code />,
+    title: "IMAMI",
+    desc: "Saya pernah menjadi Pimpinan Sidang/Presidium Musyawarah Besar IMAMI",
+  },
+  {
+    id: 6,
+    icon: <BarChart />,
+    title: "IMAPAS",
+    desc: "Saya Pernah menjadi Pemateri Sosialisasi Universitas Malikussaleh",
+  },
+]
+export const project = [
+  {
+    id: 1,
+    icon: <CloudOutlined />,
+    num: "1",
+    title: "JANGAN",
+  },
+  {
+    id: 2,
+    icon: <FavoriteBorder />,
+    num: "2",
+    title: "LUPA",
+  },
+  {
+    id: 3,
+    icon: <Public />,
+    num: "3",
+    title: "BAHAGIA",
+  },
+  {
+    id: 4,
+    icon: <PersonOutlined />,
+    num: "4",
+    title: "YA",
+  },
+]
+export const skills = [
+  {
+    id: 1,
+    cover: "../images/port/canvavaa.jpg",
+    name: "Brand",
+    category: "Beberapa",
+    title: "Brex Logo",
+  },
+  {
+    id: 2,
+    cover: "../images/port/django.jpg",
+    name: "Brand",
+    category: "Skills",
+    title: "Brex Logo",
+  },
+  {
+    id: 3,
+    cover: "../images/port/lr.jpg",
+    name: "Brand",
+    category: "Saya",
+    title: "Brex Logo",
+  },
+  {
+    id: 4,
+    cover: "../images/port/React.jpg",
+    name: "Brand",
+    category: "Beberapa",
+    title: "Brex Logo",
+  },
+  {
+    id: 5,
+    cover: "../images/port/termux.jpg",
+    name: "Brand",
+    category: "Skills",
+    title: "Brex Logo",
+  },
+  {
+    id: 6,
+    cover: "../images/port/xampp.jpg",
+    name: "Brand",
+    category: "Saya",
+    title: "Brex Logo",
+  },
+]
+export const interest = [
+  {
+    id: 1,
+    text: "Pengembangan Aplikasi Web",
+    image: "./images/interest/maye.png",
+    name: "Mayendra",
+    post: "Front End Developer",
+  },
+  {
+    id: 2,
+    text: "Sistem Informasi Manajemen (SIM)",
+    image: "./images/interest/may.png",
+    name: "Mayendra",
+    post: "Pengembang Aplikasi Bisnis",
+  },
+  {
+    id: 3,
+    text: "Design dalam aplikasi Canva",
+    image: "./images/interest/mayen.png",
+    name: "Mayendra",
+    post: "Designer",
+  },
+]
+export const awards = [
+  {
+    id: 1,
+    title: "Sebagai pemateri",
+    date: "08 Maret 2023",
+    author: "Mayendra",
+    desc: "Sertifikat ini saya peroleh pada 08 Maret 2023 sebagai Pemateri pada acara Pelatihan kesekretariatan & Kebendaharaan Kepengurusan UkM SAR UNIMAL",
+    cover: "./images/blog/Sertifikat 1.png",
+  },
+  {
+    id: 2,
+    title: "Sebagai Ketua Panitia",
+    date: "12 Mei 2023",
+    author: "Mayendra",
+    desc: "Sertifikat ini saya peroleh pada 12 Mei 2023 Sebagai Ketua Panitia Pada acara Pelantikan Kepengurusan IMAPAS Periode 2023/2024",
+    cover: "./images/blog/Sertifikat 2.png",
+  },
+  {
+    id: 3,
+    title: "Sebagai Ketua Panitia",
+    date: "30 July 2023",
+    author: "Mayendra",
+    desc: "Sertifikat ini saya peroleh pada 30 Juli 2023 sebagai Ketua Panitia Pada acara Seminar Pelatihan Kepengurusan UKM SAR UNIMAL",
+    cover: "./images/blog/Sertifikat 3.png",
+  },
+]
+export const contact = [
+  {
+    icon: <AddLocationAltOutlined />,
+    text1: "Lhokseumawe, Aceh",
+    text2: "Universitas Malikussaleh",
+  },
+  {
+    icon: <PhoneIphone />,
+    text1: "083180070248",
+    text2: "083180070248",
+  },
+  {
+    icon: <EmailOutlined />,
+    text1: "mayendraendra@gmail.com",
+    text2: "mayendraendra318@gmail.com",
+  },
+]
+export const social = [
+  {
+    icon: <Facebook />,
+  },
+  {
+    icon: <Twitter />,
+  },
+  {
+    icon: <Instagram />,
+  },
+  {
+    icon: <YouTube />,
+  },
+]
